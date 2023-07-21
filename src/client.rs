e6897204ff0fcef68b1999d10f732d26309c830a
knoxydev <nkgithubnarkuziev108@gmail.com>
2023-07-22 02:01:57 +0500

    update: possibility to send data and recieve

--- src/client.rs
+++ src/client.rs
@@ -2,34 +2,22 @@ pub mod client_md
 {
 	use std::net::{TcpStream};
 	use std::io::{Read, Write};
-	use std::str::from_utf8;
 
 	pub fn start() {
 		match TcpStream::connect("localhost:3333") {
-			Ok(mut stream) => {
+			Ok(mut stream) =>
+			{
 				println!("Successfully connected to server in port 3333");
 
-				loop {
-					let mut resp = String::from("Hello");
-					let msg: &[u8] = resp.as_bytes();
+				let mut resp = String::from("Hello");
+				let msg: &[u8] = resp.as_bytes();
+				stream.write_all(msg).unwrap();
 
-					stream.write(msg).unwrap();
-					//println!("Sent Hello, awaiting reply...");
-
-					// let mut data = [0 as u8; 6]; // using 6 byte buffer
-					// match stream.read_exact(&mut data)
-					// {
-					// 	Ok(_) => {
-					// 		if &data == msg { println!("Reply is ok!"); }
-					// 		else {
-					// 			let text = from_utf8(&data).unwrap();
-					// 			println!("Unexpected reply: {}", text);
-					// 		}
-					// 	},
-					// 	Err(e) => { println!("Failed to receive data: {}", e); }
-					// }
+				let mut data = [0; 512];
+				match stream.read(&mut data) {
+					Ok(x) => { println!("reply: {:?}", String::from_utf8_lossy(&data[..x])); },
+					Err(e) => { println!("failed to receive data: {}", e); }
 				}
-
 			},
 			Err(e) => { println!("Failed to connect: {}", e); }
 		}
