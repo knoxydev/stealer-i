666a33cd3492c0124bd1d6181696e6989c9c7864
knoxydev <nkgithubnarkuziev108@gmail.com>
2023-11-23 06:07:37 +0500

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,19 @@ mod session;
 mod client;
 pub use crate::client::client_md;
 
+mod wifi;
+pub use crate::wifi::wifi_md;
+
 
 fn main()
 {
 	println!("Hello, world!");
 
-	client_md::start(String::from("/sysinfo"));
-	client_md::start(String::from("/screen"));
-	client_md::start(String::from("/session"));
+	//client_md::start(String::from("/sysinfo"));
+	//client_md::start(String::from("/screen"));
+	//client_md::start(String::from("/session"));
+	//client_md::start(String::from("/wifi"));
+
+	let data: Vec<String> = wifi_md::start();
+	println!("{:?}", data);
 }
